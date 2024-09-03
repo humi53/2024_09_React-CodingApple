0868446b34bac480f50bc692676ae44808842937
humi53 <j.k.humi9@gmail.com>
2024-09-03 21:26:48 +0900

    map

--- blog/src/App.js
+++ blog/src/App.js
@@ -10,7 +10,8 @@ function App() {
     "강남 우동 맛집",
     "파이썬독학",
   ]);
-  let [따봉, 따봉변경] = useState(0);
+  let [따봉, 따봉변경] = useState([0, 0, 0]);
+  let [modal, setModal] = useState(false);
   return (
     <div className="App">
       <div className="black-nav">
@@ -34,17 +35,37 @@ function App() {
       >
         가나다순 정렬
       </button>
-      {글제목.map(function (content) {
+      {글제목.map(function (content, i) {
         return (
           <div className="list">
             <h4>
-              {content} <span onClick={() => 따봉변경(따봉 + 1)}>👍</span>{" "}
-              {따봉}
+              {content}{" "}
+              <span
+                onClick={() => {
+                  let copy = [...따봉];
+                  copy[i] = copy[i] + 1;
+                  따봉변경(copy);
+                }}
+              >
+                👍
+              </span>{" "}
+              {따봉[i]}
             </h4>
             <p>2월 17일 발행</p>
           </div>
         );
       })}
+      {modal == true ? <Modal /> : null}
+    </div>
+  );
+}
+
+function Modal() {
+  return (
+    <div className="modal">
+      <h4>제목</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
     </div>
   );
 }
